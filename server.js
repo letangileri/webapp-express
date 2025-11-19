bb587394035873d091d3ea87d7aca6f0f994feae
letangileri <let.angileri@gmail.com>
2025-11-19 12:40:45 +0100

    add cors middleware and update package dependencies

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const express = require("express");
 const app = express();
 const moviesRouter = require("./routes/movies") //c'è l'oggetto router che abbiamo esportato dal file movies
 const PORT = process.env.PORT || 3000;
+const cors = require('cors');
 
+app.use(cors({origin: 'http://localhost:5173'}))
 
 
 // server in ascolto
